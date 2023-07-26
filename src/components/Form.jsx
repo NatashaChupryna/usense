3ba33f85626d7b3f7e21dc2df389eb0a4e66aeae
Natasha <natkamonte1992@gmail.com>
2023-07-26 17:37:39 +0300

    fix

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   Container,
   InputForm,
@@ -6,38 +6,38 @@ import {
   Wrapper,
   StatusCard,
   EyeIcon,
-} from "./Form.styled";
-import { BsEyeSlash, BsEye } from "react-icons/bs";
+} from './Form.styled';
+import { BsEyeSlash, BsEye } from 'react-icons/bs';
 
 const Form = () => {
-  const [password, setPassword] = useState("");
-  const [type, setType] = useState("password");
+  const [password, setPassword] = useState('');
+  const [type, setType] = useState('password');
   const [toggleIconPass, setToggleIconPass] = useState(<BsEyeSlash />);
 
   const togglePassInput = () => {
-    if (type === "password") {
-      setType("text");
+    if (type === 'password') {
+      setType('text');
       setToggleIconPass(<BsEye />);
     } else {
-      setType("password");
+      setType('password');
       setToggleIconPass(<BsEyeSlash />);
     }
   };
 
   const passStrength = () => {
     if (password.length === 0) {
-      return "#cecaca";
+      return '#cecaca';
     }
     if (password.length < 8) {
-      return "#ee3d3d";
+      return '#ee3d3d';
     }
 
     const hasLetters = /[A-Za-z]/.test(password);
     const hasDigits = /\d/.test(password);
-    const hasSymbols = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+    const hasSymbols = /[!@#$%^&*()_+\-=[\]{};':"|,.<>/?]/.test(password);
 
     if (hasLetters && hasDigits && hasSymbols) {
-      return "#44c344";
+      return '#44c344';
     }
 
     if (
@@ -45,10 +45,10 @@ const Form = () => {
       (hasLetters && hasSymbols) ||
       (hasDigits && hasSymbols)
     ) {
-      return "#ffff66";
+      return '#ffff66';
     }
 
-    return "#ee3d3d";
+    return '#ee3d3d';
   };
 
   const sectionColor = passStrength();
@@ -64,7 +64,7 @@ const Form = () => {
           variant="outlined"
           type={type}
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={e => setPassword(e.target.value)}
         />
 
         <EyeIcon type="button" onClick={togglePassInput}>
@@ -74,13 +74,13 @@ const Form = () => {
           <StatusCard
             style={{
               backgroundColor:
-                sectionColor === "#ee3d3d"
-                  ? "#ee3d3d"
-                  : sectionColor === "#ffff66"
-                  ? "#ffff66"
-                  : sectionColor === "#44c344"
-                  ? "#44c344"
-                  : "#cecaca",
+                sectionColor === '#ee3d3d'
+                  ? '#ee3d3d'
+                  : sectionColor === '#ffff66'
+                  ? '#ffff66'
+                  : sectionColor === '#44c344'
+                  ? '#44c344'
+                  : '#cecaca',
             }}
           >
             easy
@@ -88,13 +88,13 @@ const Form = () => {
           <StatusCard
             style={{
               backgroundColor:
-                sectionColor === "#ee3d3d" && password.length < 8
-                  ? "#ee3d3d"
-                  : sectionColor === "#ffff66"
-                  ? "#ffff66"
-                  : sectionColor === "#44c344"
-                  ? "#44c344"
-                  : "#cecaca",
+                sectionColor === '#ee3d3d' && password.length < 8
+                  ? '#ee3d3d'
+                  : sectionColor === '#ffff66'
+                  ? '#ffff66'
+                  : sectionColor === '#44c344'
+                  ? '#44c344'
+                  : '#cecaca',
             }}
           >
             medium
@@ -102,11 +102,11 @@ const Form = () => {
           <StatusCard
             style={{
               backgroundColor:
-                sectionColor === "#ee3d3d" && password.length < 8
-                  ? "#ee3d3d"
-                  : sectionColor === "#44c344"
-                  ? "#44c344"
-                  : "#cecaca",
+                sectionColor === '#ee3d3d' && password.length < 8
+                  ? '#ee3d3d'
+                  : sectionColor === '#44c344'
+                  ? '#44c344'
+                  : '#cecaca',
             }}
           >
             strong
